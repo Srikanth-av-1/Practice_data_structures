1bcaf50b964158773493e56c7fd031d13a4c43be
Srikanth-av-1 <68683754+Srikanth-av-1@users.noreply.github.com>
2021-02-22 12:03:05 +0530

    Add files via upload

--- Python_data_structures.py
+++ Python_data_structures.py
@@ -1,8 +1,9 @@
 class Node:
     def __init__(self,data):
         self.data = data
-        self.ref = None
-class LinkedList:
+        self.pref = None
+        self.nref = None
+class doublyLinkedList:
     def __init__(self):
         self.head = None
     def printLL(self):
@@ -12,85 +13,115 @@ class LinkedList:
             n = self.head
             while n is not None:
                 print(n.data,end='--->')
-                n = n.ref
+                n = n.nref
+    def print_reverse(self):
+        print()
+        if self.head is None:
+            print('Linked List is empty')
+        else:
+            n = self.head
+            while n.nref is not None:
+                n = n.nref
+            while n is not None:
+                print(n.data,end='<---')
+                n = n.pref
     def add_begin(self,data):
         new_node = Node(data)
-        new_node.ref = self.head
-        self.head = new_node
+        if self.head is None:
+            new_node.nref = self.head
+            #new_node.nref.pref = new_node
+            self.head = new_node
+        else:
+            new_node.nref = self.head
+            new_node.nref.pref = new_node
+            self.head = new_node
     def add_end(self,data):
         new_node = Node(data)
         if self.head is None:
+            new_node.nref = self.head
             self.head = new_node
         else:
             n = self.head
-            while n.ref is not None:
-                n = n.ref
-            n.ref = new_node
+            while n.nref is not None:
+                n = n.nref
+            n.nref = new_node
+            new_node.pref = n
     def add_after_gvn_node(self,data,data1):
         n = self.head
-        while n.ref is not None:
+        while n.nref is not None:
             if n.data == data:
                 break
-            n = n.ref
+            n = n.nref
         if n is None:
             print('node not found')
         else:
             new_node = Node(data1)
-            new_node.ref = n.ref
-            n.ref = new_node
+            new_node.nref = n.nref
+            n.nref.pref = new_node
+            new_node.pref = n
+            n.nref = new_node
     def add_before_gvn_node(self,data,data1):
         if self.head is None:
             print('Linked List is empty')
             return
         if self.head.data == data:
             new_node = Node(data1)
-            new_node.ref = self.head
+            new_node.nref = self.head
+            self.head.pref = new_node
             self.head = new_node
             return
         n = self.head
-        while n.ref is not None:
-            if n.ref.data == data:
+        while n.nref is not None:
+            if n.nref.data == data:
                 break
-            n = n.ref
-        if n.ref is None:
+            n = n.nref
+        if n.nref is None:
             print('Node not found')
         else:
             new_node = Node(data1)
-            new_node.ref = n.ref
-            n.ref = new_node
+            new_node.nref = n.nref
+            n.nref.pref = new_node
+            n.nref = new_node
+            new_node.pref = n
     def del_first_node(self):
         if self.head is None:
             print('Linked List is empty deletion not possible')
         else:
-            self.head = self.head.ref
+            self.head = self.head.nref
+            self.head.nref.pref = self.head
+
     def del_last_node(self):
         if self.head is None:
             print('Linked List is empty deletion not possible')
         else:
             n = self.head
-            while (n.ref).ref is not None:
-                n = n.ref
-            n.ref = n.ref.ref
+            while n.nref.nref is not None:
+                n = n.nref
+            n.nref = n.nref.nref
+            #n.nref.pref = n
     def del_given_node(self,data):
         if self.head is None:
             print('Linked List is empty deletion not possible')
         else:
             n = self.head
-            while n.ref.data != data:
-                n = n.ref
-            n.ref = n.ref.ref
+            while n.nref.data != data:
+                n = n.nref
+            n.nref = n.nref.nref
+            n.nref.pref = n
 
 
 
 
-a = LinkedList()
-a.add_begin(10)
-a.add_end(20)
-a.add_end(30)
-a.add_after_gvn_node(10,15)
-a.add_before_gvn_node(15,12)
-#a.del_first_node()
-#a.del_last_node()
-a.del_given_node(15)
+
+a = doublyLinkedList()
+a.add_begin(50)
+a.add_begin(40)
+a.add_begin(30)
+a.add_end(60)
+a.add_after_gvn_node(30,35)
+a.add_before_gvn_node(50,45)
+a.del_last_node()
+a.del_given_node(40)
 a.printLL()
+a.print_reverse()
 
